f42458b0499cabeda3f652b5daf868642061920e
cipher-sphere <shivam0hapur@gmail.com>
2026-03-18 10:05:57 +0530

    done

--- Assignment6_17March/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package Assignment6_17March;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "Assignment6_17March")
-public class AppConfig {
-}
